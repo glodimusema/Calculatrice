396915f8cbafd2e414b5288d1d85062c6e27b108
glodimusema <glodimaley@isig.ac.cd>
2025-03-25 13:06:48 +0200

    une classe

---
+++ Calculatrice/clsGlossiaire.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculatrice
+{
+    class clsGlossiaire
+    {
+        connexion cnx = null;
+        private static clsGlossiaire glos;
+        SqlConnection con = null;
+        SqlCommand cmd = null;
+        SqlDataAdapter dt = null;
+        SqlDataReader dr = null;
+
+        public void innitialiseConnect()
+        {
+            try
+            {
+                cnx = new connexion(); cnx.connect();
+                con = new SqlConnection(cnx.chemin);
+            }
+            catch (Exception)
+            {
+                throw new Exception("l'un de vos fichiers de configuration est incorrect");
+            }
+
+        }
+        public static clsGlossiaire GetInstance()
+        {
+            if (glos == null)
+                glos = new clsGlossiaire();
+            return glos;
+        }
+        private static void setParameter(SqlCommand cmd, string name, DbType type, int length, object paramValue)
+        {
+            IDbDataParameter a = cmd.CreateParameter();
+            a.ParameterName = name;
+            a.Size = length;
+            a.DbType = type;
+
+            if (paramValue == null)
+            {
+                if (!a.IsNullable)
+                {
+                    a.DbType = DbType.String;
+                }
+                a.Value = DBNull.Value;
+            }
+            else
+                a.Value = paramValue;
+            cmd.Parameters.Add(a);
+        }
+
+        public DataTable loadData(string nomTable)
+        {
+
+            innitialiseConnect();
+            if (!con.State.ToString().ToLower().Equals("open")) con.Open();
+            DataTable table = new DataTable();
+            dt = new SqlDataAdapter("select * from "+nomTable+"", con);
+            dt.Fill(table);
+            con.Close();
+
+            return table;
+        }
+
+        public void loadCombo(string nomTable,string nomchamp,System.Windows.Forms.ComboBox comb1)
+        {
+            innitialiseConnect();
+            if (!con.State.ToString().ToLower().Equals("open")) con.Open();
+            DataTable table = new DataTable();
+            dt = new SqlDataAdapter("SELECT "+nomchamp+" FROM "+nomTable+"", con);
+            try
+            {
+                DataTable dt1 = new DataTable();
+                dt.Fill(dt1);
+
+                foreach (DataRow dr in dt1.Rows)
+                {
+                    comb1.Items.Add(dr["nomEcol"]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur" + ex);
+            }
+
+            con.Close();
+        }
+
+        public string getcode_Combo(string nomTable,string nomChampId, string nomChamp,string valeur)
+        {
+            string IdData = "";
+            try
+            {                
+                innitialiseConnect();
+                if (!con.State.ToString().ToLower().Equals("open")) con.Open();
+                cmd = new SqlCommand("select " + nomChampId + " from " + nomTable + " where " + nomChamp + "=@a", con);
+                cmd.Parameters.AddWithValue("@a", valeur);
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    IdData = (dr[nomChampId].ToString());
+                }
+                cmd.Dispose();
+                con.Close();                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return IdData;
+        }
+
+        public DataSet get_Report_All(string nomTable)
+        {
+            DataSet dst;
+            try
+            {
+                innitialiseConnect();
+                if (!con.State.ToString().ToLower().Equals("open")) con.Open();
+                cmd = new SqlCommand("SELECT * FROM " + nomTable + "", con);
+                dt = new SqlDataAdapter(cmd);
+                dst = new DataSet();
+                dt.Fill(dst, nomTable);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                dt.Dispose(); con.Close();
+            }
+            return dst;
+        }
+        public DataSet get_Report_Trier(string nomTable, string nomchamp, DateTime val1, DateTime val2)
+        {
+            DataSet dst;
+            try
+            {
+                innitialiseConnect();
+                if (!con.State.ToString().ToLower().Equals("open")) con.Open();
+                cmd = new SqlCommand("SELECT * FROM " + nomTable + " WHERE " + nomchamp + " between @date1 and @date2 ", con);
+                setParameter(cmd, "@date1", DbType.DateTime, 30, val1);
+                setParameter(cmd, "@date2", DbType.DateTime, 30, val2);
+                dt = new SqlDataAdapter(cmd);
+                dst = new DataSet();
+                dt.Fill(dst, nomTable);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                dt.Dispose(); con.Close();
+            }
+            return dst;
+        }
+
+        public DataSet get_Report_One(string nomTable, string nomchamp, string val1)
+        {
+            DataSet dst;
+            try
+            {
+                innitialiseConnect();
+                if (!con.State.ToString().ToLower().Equals("open")) con.Open();
+                cmd = new SqlCommand("SELECT * FROM " + nomTable + " WHERE " + nomchamp + " = @val1 ", con);
+                setParameter(cmd, "@val1", DbType.String, 30, val1);
+                dt = new SqlDataAdapter(cmd);
+                dst = new DataSet();
+                dt.Fill(dst, nomTable);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                dt.Dispose(); con.Close();
+            }
+            return dst;
+        }
+    }
+}
---
+++ Calculatrice/connexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculatrice
+{
+    class connexion
+    {
+        public string chemin;
+
+        public void connect()
+        {
+            chemin = File.ReadAllText("").Trim();
+        }
+    }
+}
